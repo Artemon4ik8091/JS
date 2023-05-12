fb621c9692f22c76fbc5418af3187f7474c5c693
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2023-05-12 20:07:17 +0300

    base commands

---
+++ lesson1/script.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    // var fname = prompt("Как вас зовут?");
+    // var lname = prompt("Как ваша фамилия?");
+    
+    // firstname.innerHTML = fname;
+    // lastname.innerHTML = lname;
+
+    console.log(firstname.innerHTML);
+    console.log(lastname.innerHTML);
+});
+
