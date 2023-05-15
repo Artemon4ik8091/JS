24b49386f6c15948ae4cdd0ba35259f6d1f9b97e
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2023-05-15 19:06:04 +0300

    task2 end

--- lesson3/script.js
+++ lesson3/script.js
@@ -1,3 +1,19 @@
 window.addEventListener("DOMContentLoaded", (event) => {
    
+    //task1
+    var count = 10;
+    const symbol = "#";
+    while (count > 0)
+    {
+        loger.innerHTML += symbol;
+        count--;
+    }
+    //task2
+    var count2 = 10;
+    while (count2 > 0)
+    {
+        loger.innerHTML += "<p>" + count2 + "</p>";
+        count2--;
+    }
+    
 });
