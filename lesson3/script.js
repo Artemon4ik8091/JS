53ec6b2fcf34f3360a8d6ed6e606061d20abe6ab
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2023-05-15 19:46:46 +0300

    lesson 3 practice end

--- lesson3/script.js
+++ lesson3/script.js
@@ -1,19 +1,114 @@
 window.addEventListener("DOMContentLoaded", (event) => {
    
-    //task1
-    var count = 10;
-    const symbol = "#";
-    while (count > 0)
-    {
-        loger.innerHTML += symbol;
-        count--;
-    }
-    //task2
-    var count2 = 10;
-    while (count2 > 0)
-    {
-        loger.innerHTML += "<p>" + count2 + "</p>";
-        count2--;
-    }
+    // //task1
+    // var count = 10;
+    // const symbol = "#";
+    // while (count > 0)
+    // {
+    //     loger.innerHTML += symbol;
+    //     count--;
+    // }
+    // //task2
+    // var count2 = 10;
+    // while (count2 > 0)
+    // {
+    //     loger.innerHTML += "<p>" + count2 + "</p>";
+    //     count2--;
+    // }
+
+    // //task3
+    // var number = 10;
+    // var pow = 5;
+    // while (pow > 0)
+    // {
+    //     number *= 10;
+    //     pow--;
+    // }
+    // console.log(number);
     
+    //  //task3
+    //  var number = 10;
+    //  var pow = 5;
+    //  while (pow > 0)
+    //  {
+    //      number *= 10;
+    //      pow--;
+    //  }
+    //  console.log(number);
+
+    //task4
+    // var startDivider = 1;
+    // var number1 = 130;
+    // var number2 = 65;
+    // var massive = []; 
+    // while (startDivider <= Math.min(number1,number2))
+    // {
+    //     if (number1 % startDivider == 0 && number2 % startDivider == 0)
+    //         massive.push(startDivider);
+    //     startDivider++;
+    // }    
+    // console.log(massive);
+
+    // //task5
+    // var startDivider = 1;
+    // var number1 = 13;
+    // var factorial = 1;
+
+    // while (startDivider <= number1)
+    // {
+    //     factorial *= startDivider;
+    //     startDivider++;
+    // }   
+    // console.log(factorial);
+
+    //task6
+    // do
+    // {
+    //     alert("Решите пример 2+2*2");
+    //     var answer = prompt();
+    // }
+    // while(answer != 6)
+
+    // //task7
+    // var start = 1000;
+    // var limiter = 50;
+    // var divider = 2;
+    // var countDivide = 0;
+    // do
+    // {
+    //     start = start / divider;
+    //     countDivide++;
+    //     console.log("Деление номер " + countDivide);
+    //     console.log("Полученное значение " + start);
+    // }
+    // while (start >= limiter)
+
+    // //task8
+    // var number = 63;
+    // for (var i = 1; i <= 100;i++)
+    // {
+    //     if (number % i == 0)
+    //         console.log(i);
+    // }
+
+    // //task9
+    // var number1 = 20;
+    // var number2 = 93;
+    // for (var i = number1+4; i <= number2;i+=4)
+    // {
+    //     console.log(i);
+    // }
+
+    // //task10
+    // var number1 = prompt();
+    // var prime = true;
+    // for (var i = 2; i <= number1/2;i++)
+    // {        
+    //     if (number1 % i == 0)
+    //     {
+    //         prime = false;
+    //         break;
+    //     }            
+    // }
+    // console.log(prime);
 });
