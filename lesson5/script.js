31ae3ef2741712ab6229e68a50d2bbe1178fa41a
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2023-05-22 19:21:41 +0300

    lesson 5, task 1-3

--- lesson5/script.js
+++ lesson5/script.js
@@ -1,251 +1,51 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    // 1. Написать функцию, которая принимает 2 числа и возвращает меньшее из них.
-    // console.log(LessNumber(20,15));
-
-    // 2. Написать функцию, которая возводит переданное число в указанную степень
-    // console.log(NumberPow(2,5));
-
-    // 3. Написать функцию, которая принимает 2 числа и знак (+ - * /), считает пример и возвращает результат.
-    // console.log(Calculate(10,5,'+'));
-    // console.log(Calculate(10,5,'-'));
-    // console.log(Calculate(10,5,'/'));
-    // console.log(Calculate(10,5,'*'));
-    // console.log(Calculate(10,0,'/'));
-    // console.log(Calculate(10,0,'^'));
-
-    // 4. Написать функцию, которая проверяет, является ли переданное ей число простым.
-    // console.log(IsPrimeNumber(7));
-    // console.log(IsPrimeNumber(-7));
-    // console.log(IsPrimeNumber("7"));
-    // console.log(IsPrimeNumber(undefined));
-    // console.log(IsPrimeNumber(null));
-
-    // 5. Написать функцию, которая принимает число и выводит таблицу умножения для этого числа. Вызовите функцию для всех чисел от 2 до 9.
-    // MultiplyTable(2);
-    // MultiplyTable(3);
-    // MultiplyTable(4);
-    // MultiplyTable(5);
-    // MultiplyTable(6);
-    // MultiplyTable(7);
-    // MultiplyTable(8);
-    // MultiplyTable(9);
-
-    // 6. Написать функцию, которая реализует работу оператора %. 
-    // Функция принимает 2 параметра и возвращает остаток от деления первого параметра на второй. 
-    // В функции использовать только + - * /, оператор % не использовать. 
-    // console.log(RemainderOfTheDivision(274586, 10));
-    // console.log(RemainderOfTheDivision(274586, 15));
-    // console.log(RemainderOfTheDivision(274586, 98481652));
-    // console.log(RemainderOfTheDivision(0, 0));
-    // console.log(RemainderOfTheDivision(274586, 387));
-
-    // 7. Написать функцию, которая принимает от 1 до 5 чисел и возвращает их сумму.
-    // console.log(Summator (20));
-    // console.log(Summator (20,20,20,20,20));
-
-    // 8. Написать функцию, которая принимает от 1 до 5 чисел и возвращает большее из них.
-    // console.log(MaxNumber(20,30,40,10,0));
-    // console.log(MaxNumber(-20,-30,-40,-10,-5));
-    // 9. Написать функцию, которая выводит все четные или нечетные числа, в указанном пользователем диапазоне. 
-    // Какие числа выводить, определяется третьим параметром типа bool (true – четные, false – нечетные).
-    // OddEvenNumbers(20,50,true);
-    // OddEvenNumbers(20,50);
-
-    // 10. Написать функцию, которая принимает дату (день, месяц, год) и возвращает дату следующего дня в виде строки «дд.мм.гггг». 
-    // Проверку на високосный год желательно написать отдельной функцией.
-
-    // console.log(NextDayDate(20,05,2032));
-    // console.log(NextDayDate(28,2,2028));
-
-    // console.log(ConcatNumbers(1,2,3)); //DZ3
-    // console.log(IsSuperNumber(6)); //DZ5
-    // console.log(IsSuperNumber(28));
-    // console.log(IsSuperNumber(496));
+    //console.log(FactorialRecursive(5)); //Task1
+    // OutputInRangeDesc(2,10);  //Task2
+    // OutputInRangeAsc(2,10);  //Task2
+    // console.log(ReverseOutput(2345)); //Task3
 });
 
-function LessNumber(number1,number2) {  //Task1
-    return number1 > number2 ? number2 : number1;
-}
-
-function NumberPow(number, pow) { //Task2
-    let answer = number;
-    for(var i = 1; i < pow; i++)
+function FactorialRecursive(number, result = 1) //Task1
+{
+    if (number == 0) 
     {
-        answer *= number;
+        return result;
     }
-    return answer;
+    result = number * FactorialRecursive(number-1, result);
+    return result;  
 }
 
-function Calculate(number1, number2, sign) { //Task3
-    if (sign === "/" && number2 == 0)
+function OutputInRangeDesc (min,max) //Task2
+{
+    if (min == max)
     {
-        return "ZeroDivideException";
+        console.log(max);
+        return;
     }
-    switch(sign)
+    console.log(max);
+    OutputInRangeDesc(min, max-1);
+}
+function OutputInRangeAsc (min, max) //Task2
+{
+    if (min == max)
     {
-        case "+": return number1 + number2;
-        case "-": return number1 - number2;
-        case "/": return number1 / number2;
-        case "*": return number1 * number2;
-        default: return "Unknown operation";
+        console.log(min);
+        return;
     }
+    console.log(min);
+    OutputInRangeAsc(min+1, max);
 }
 
-function IsPrimeNumber(number) { //Task4
-    if (number < 2) return "InvalidRangeException";
-    if (number === undefined) return "BadArgumentFunctionException";
-    for (let i = 2; i < number; i++)
-    {
-        if(number % i == 0)
-            return false;
+function ReverseOutput(number)  //Task3
+{
+    number = number.toString();
+    if(number.length > 0 )
+    { 
+        return Number(number.substring(number.length-1) + 
+        ReverseOutput(number.substring(0,number.length-1)));
     }
-    return true;
-}
-
-function MultiplyTable (number) { //Task5
-    for (let i = 1; i < 10; i++)
-    {
-        console.log(`${number} * ${i} = ` +  number * i);
-    }
-    console.log("");
-}
-
-function RemainderOfTheDivision (number, divider) {  //Task6
-    if (divider == 0 || number == 0) return "badrequest";
-    if (divider > number) return number + " bad";
-    let remainder = Math.trunc(number / divider); 
-    remainder = number - remainder*divider;
-    return remainder;
-}
-
-function Summator() { //Task7
-    if (arguments.length >= 1 && arguments.length <=5)
-    {
-        let sum = 0;
-        for(let i = 0; i < arguments.length; i++ )
-        {
-            sum += arguments[i];
-        }
-        return sum;
-    }
-    return 0;    
-}
-
-function MaxNumber() { //Task8
-    let max = 0;
-    if (arguments.length >= 1 && arguments.length <= 5)
-    {        
-        max = arguments[0];
-        for(let i = 0; i < arguments.length; i++ )
-        {
-            if(arguments[i] > max)
-                max = arguments[i];
-        }        
-    }
-    return max;   
-}
-
-function OddEvenNumbers(startRange, endRange, even = false) { //Task9
-    for(let i = startRange; i <= endRange; i++)
-    {
-        if (even)
-        {
-            if (i % 2 == 0) 
-                console.log(i);
-        }
-        else
-        {
-            if (i % 2 != 0) 
-                console.log(i);
-        }
-    }
-}
-
-function NextDayDate(day,month,year) { //Task10
-    let errorDate = false;
-    let longMonth = [1,3,5,7,8,10,12];
-    let shortMonth = [4,6,9,11];
-    let leapYear = false;
-    
-    errorDate = year > 0 && year <= 9998 ? false : true;
-    if (!errorDate)
-    {        
-        leapYear = IsLeapYear(year);
-    }
-    console.log(leapYear);
-    errorDate = month > 0 && month <= 12 ? false : true;
-    if (!errorDate) 
-    {   
-        errorDate = day > 0 && day <= 31 && longMonth.includes(month) ? false : true;
-        errorDate = day > 0 && day <= 30 && shortMonth.includes(month) ? false : true;
-        if (leapYear)
-        {
-            errorDate = day > 0 && day <= 29 && month == 2  ? false : true;
-        }
-        else
-        {
-            errorDate = day > 0 && day <= 28 && month == 2 ? false : true;
-        }     
-    }
-
-    day = day + 1;
-
-    if (day <= 31 && longMonth.includes(month) || 
-        day <= 30 && shortMonth.includes(month) || 
-        day <= 29 && month == 2 && leapYear ||  
-        day <= 28 && month == 2)
-        return day + "." + month + "." + year;
     else
     {
-        day = 1;
-        month = month + 1;
-        if (month <= 12)
-        {
-            return day + "." + month + "." + year;
-        }
-        else
-        {
-            month = 1;
-            year++;
-            return day + "." + month + "." + year;
-        }
+        return "";
     }
-}
-
-function IsLeapYear(year) { //Task10 InnerFunction
-    let leapYear = false;
-    if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            if (year % 400 == 0) {
-                leapYear = true;
-            }                
-        }
-        else {
-            leapYear = true;
-        }            
-    }
-    return leapYear;
-}
-
-
-function ConcatNumbers (n1,n2,n3) //DZ 3
-{
-    return Number(n1 + "" + n2 + "" + n3);
-}
-
-function IsSuperNumber(number) {
-    let divisionsMassive = [];
-    let sum = 0;
-    for (let i = 1; i < number; i++) {
-        if (number % i == 0) {
-            divisionsMassive.push(i);
-        }
-    }
-    for (let i = 0; i < divisionsMassive.length; i++) {
-        sum += divisionsMassive[i];
-    }
-    if (number == sum) {
-        return true;
-    }    
-    return false;
 }
