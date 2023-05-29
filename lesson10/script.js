536981102d3e6c679e43d4b2a41666df2cc7a2dd
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2023-05-29 20:30:15 +0300

    lesson10 task1 added

--- lesson10/script.js
+++ lesson10/script.js
@@ -1,3 +1,36 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    
+    var btn = new Button(300,60,"Узнать подробнее");
+    btn.ShowBtn();
+    var btn2 = new BootstrapButton("red",300,60,"Узнать подробнее");
+    btn2.ShowBtn();
 });
+
+class Button {
+    constructor(width, height, text) {
+        this.width = width;
+        this.height = height;
+        this.text = text;
+    }
+    ShowBtn() {
+        let button = "<button ";
+        button += `style="width:${this.width}; height:${this.height};"`;
+        button +=">"
+        button += this.text;
+        button += "</button>";
+        document.write(button);
+    }
+}
+class BootstrapButton extends Button {
+    constructor(color,width,height,text) {
+        super(width,height,text);
+        this.color = color;        
+    }
+    ShowBtn() {
+        let button = "<button ";
+        button += `style="width:${this.width}; height:${this.height}; background:${this.color};"`;
+        button +=">"
+        button += this.text;
+        button += "</button>";
+        document.write(button);
+    }
+}
