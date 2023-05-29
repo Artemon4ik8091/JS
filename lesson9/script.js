74ff05c11a3671a092cab06bd47d41672c54dae7
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2023-05-29 19:52:07 +0300

    dz2 added lesson 9

--- lesson9/script.js
+++ lesson9/script.js
@@ -38,16 +38,40 @@ window.addEventListener("DOMContentLoaded", (event) => {
     var article5 = new Article("Мой заголовок5","Небольшой текст моей статьи 5 ", ["fruit","news"],new Date(1540000000000)); //task2
     //article.print();
 
-    var news  = new News([article,article2,article3,article4]);  //task3
-    console.log(news.length);
-    //news.printAllNews();
-    news.AddNews(article5);
-    news.DeleteNews("Мой заголовок3");
-    //news.printAllNews();
-    news.Sort();
-    //news.printAllNews();
-    var oldNews = new News(news.SearchByTag("old"));
-    oldNews.printAllNews();
+    // var news  = new News([article,article2,article3,article4]);  //task3
+    // console.log(news.length);
+    // //news.printAllNews();
+    // news.AddNews(article5);
+    // news.DeleteNews("Мой заголовок3");
+    // //news.printAllNews();
+    // news.Sort();
+    // //news.printAllNews();
+    // var oldNews = new News(news.SearchByTag("old"));
+    // oldNews.printAllNews();
+
+    let wrapper = new HtmlElement("div",false,"");
+    wrapper.SetAttr("id","wrapper");
+    wrapper.SetStyle("display","flex");
+    let link = new HtmlElement("a",false,"More...");
+    link.SetAttr("href","https://www.lipsum.com/");
+    link.SetAttr("target","_blank");
+    let paragraph = new HtmlElement("p",false,"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.");
+    paragraph.SetStyle("text-align","justify");
+    paragraph.AddTagInEnd(link);
+    let image = new HtmlElement("img",true,"");
+    image.SetAttr("src","lipsum.jpg");
+    image.SetAttr("alt","Lorem Ipsum");
+    image.SetStyle("width","100%");
+    let header = new HtmlElement("h3", false, "What is Lorem Ipsum?");
+    let anonimDiv = new HtmlElement("div",false,"");
+    anonimDiv.SetStyle("width","300px");
+    anonimDiv.SetStyle("margin","10px");
+    anonimDiv.AddTagInEnd(image);
+    anonimDiv.AddTagInEnd(paragraph);
+    anonimDiv.AddTagInStart(header);
+    wrapper.AddTagInEnd(anonimDiv);
+    wrapper.AddTagInEnd(anonimDiv);
+    document.write(wrapper.GetHtml());
 });
 // function HelloWorld() {
 //     alert("Hello world!");
@@ -174,5 +198,68 @@ class News {  //task3
                 newArticles.push(article);
         });
         return newArticles;
+    }
+}
+
+
+class HtmlElement {
+    constructor(tagName,selfClosing,text) {
+        this.tagName = tagName;
+        this.selfClosing = selfClosing;
+        this.text = text;
+        this.attrs = [];
+        this.styles = [];
+        this.tags = [];
+    }
+    SetAttr(attr,value) {
+        let attribute = { 
+            attribute:attr,
+            value:value
+        }
+        this.attrs.push(attribute);
+    }
+    SetStyle(key,value) {
+        let style = { 
+            key:key,
+            value:value
+        }
+        this.styles.push(style);
+    }
+    AddTagInStart(HtmlElement) {
+        this.tags.unshift(HtmlElement);
+    }
+    AddTagInEnd(HtmlElement) {
+        this.tags.push(HtmlElement);
+    }
+
+    GetHtml() {
+        let htmlString = "";
+        htmlString += `<${this.tagName} `;
+        if(this.attrs.length > 0) {
+            for(let i = 0; i < this.attrs.length; i++) {
+                htmlString += `${this.attrs[i].attribute}="${this.attrs[i].value}" `;
+            }
+        }
+        if(this.styles.length > 0) {
+            htmlString += `style=" `;
+            for(let i = 0; i < this.styles.length; i++) {
+                htmlString += `${this.styles[i].key}:${this.styles[i].value}; `;
+            }
+            htmlString += `"`;
+        }
+        htmlString += `>`;
+        if(this.text.length > 0)
+            htmlString += this.text;
+        
+        if(this.tags.length > 0) {
+            for(let i = 0; i < this.tags.length; i++) {
+                htmlString += this.tags[i].GetHtml();
+            }
+        }
+        if(this.selfClosing !== true) {
+            htmlString += `</${this.tagName}>`;
+        }
+
+        return htmlString;
     }
 }
